36eff2eba64363b19eee19d99106e9c94a0e4e5b
Rodrigo Gracas <rda.gracas@hotmail.com>
2018-09-26 21:25:50 +0100

    navigate between pages
    Here we've looked at the very basic usage of 'next/link'.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+// Link API
+import Link from 'next/link'
+
 const Index = () => (
     <div>
+        <Link href="/about">
+            <a style={{ fontSize: 20 }}>About page</a>
+        </Link>
+
+        {/* Link with a button */}
+        <Link href="/about">
+            <button>Go to about page</button>
+        </Link>
         <p>Hello Next.js</p>
     </div>
 )
